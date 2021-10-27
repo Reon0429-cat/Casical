d52a8f4173dcb7e8bea4e7f6bfad40622c27fded
Reon0429-cat <oonishireon110@gmail.com>
2021-10-27 22:05:19 +0900

    first commit

---
+++ Casical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Casical
+//
+//  Created by 大西玲音 on 2021/10/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
