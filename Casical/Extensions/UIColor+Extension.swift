9bfc8d3866fc8cf2bfb843684957e6a7ecffee02
Reon0429-cat <oonishireon110@gmail.com>
2021-10-29 04:07:14 +0900

    Loginのレイアウト作成

---
+++ Casical/Extensions/UIColor+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+Extension.swift
+//  Casical
+//
+//  Created by 大西玲音 on 2021/10/29.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var lightColor: UIColor {
+        UIColor(named: "lightColor")!
+    }
+    
+    static var darkColor: UIColor {
+        UIColor(named: "darkColor")!
+    }
+    
+}
---
+++ Casical/Login/LoginViewController.swift
@@ -0,0 +1,51 @@
+//
+//  LoginViewController.swift
+//  Casical
+//
+//  Created by 大西玲音 on 2021/10/29.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    @IBOutlet private weak var mailAddressTextField: CustomTextField!
+    @IBOutlet private weak var passwordTextField: CustomTextField!
+    @IBOutlet private weak var loginButton: UIButton!
+    @IBOutlet private weak var signUpButton: UIButton!
+    @IBOutlet private weak var twitterLoginButton: UIButton!
+    @IBOutlet private weak var googleLoginButton: UIButton!
+    @IBOutlet private weak var appleLoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mailAddressTextField.backgroundColor = .lightColor
+        passwordTextField.backgroundColor = .lightColor
+        loginButton.backgroundColor = .darkColor
+        twitterLoginButton.layer.borderColor = UIColor.lightColor.cgColor
+        twitterLoginButton.layer.borderWidth = 1
+        googleLoginButton.layer.borderColor = UIColor.lightColor.cgColor
+        googleLoginButton.layer.borderWidth = 1
+        appleLoginButton.layer.borderColor = UIColor.lightColor.cgColor
+        appleLoginButton.layer.borderWidth = 1
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        mailAddressTextField.roundCorners()
+        passwordTextField.roundCorners()
+        loginButton.layer.cornerRadius = loginButton.frame.height / 2
+        twitterLoginButton.layer.cornerRadius = twitterLoginButton.frame.height / 2
+        googleLoginButton.layer.cornerRadius = googleLoginButton.frame.height / 2
+        appleLoginButton.layer.cornerRadius = appleLoginButton.frame.height / 2
+        
+    }
+    
+    @IBAction private func loginButtonDidTapped(_ sender: Any) {
+        
+    }
+    
+}
