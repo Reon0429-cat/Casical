e495c6032b76886187a13fb97d8e3451146fbddb
Reon0429-cat <oonishireon110@gmail.com>
2021-10-29 20:51:34 +0900

    Homeレイアウト実装

--- Casical/Home/HomeViewController.swift
+++ Casical/Home/HomeViewController.swift
@@ -8,16 +8,50 @@
 import UIKit
 import FirebaseAuth
 
+struct SampleModel {
+    let image: UIImage
+    let name: String
+    let language: String
+    let house: String
+    let experience: String
+}
+
+private extension SampleModel {
+    static let data = [SampleModel(image: UIImage(named: "reon")!, name: "OONISHI REON", language: "Swift", house: "東京", experience: "1年"),
+                       SampleModel(image: UIImage(named: "adu")!, name: "Azuki Yamada", language: "CSS", house: "大阪", experience: "6ヶ月"),
+                       SampleModel(image: UIImage(named: "sakura")!, name: "宮脇 咲良", language: "JavaScript", house: "鹿児島", experience: "5年6ヶ月"),
+                       SampleModel(image: UIImage(named: "mai")!, name: "Mai Shiraishi", language: "Java", house: "群馬", experience: "中途未経験"),
+                       SampleModel(image: UIImage(named: "asuka")!, name: "齋藤 飛鳥", language: "PHP", house: "東京", experience: "新卒未経験"),]
+}
+
 final class HomeViewController: UIViewController {
+    
+    @IBOutlet private weak var collectionView: UICollectionView!
+    @IBOutlet private weak var sortScoreButton: UIButton!
+    @IBOutlet private weak var sortRegisterButton: UIButton!
+    @IBOutlet private weak var sortExperienceButton: UIButton!
+    @IBOutlet private weak var sortButtonFollowView: UIView!
+    @IBOutlet private weak var filterButton: UIButton!
+    @IBOutlet private weak var settingButton: UIButton!
+    
+    private let sampleData = [[SampleModel]](repeating: SampleModel.data, count: 10).flatMap { $0 }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        try! Auth.auth().signOut()
         let isNotLoggedIn = (Auth.auth().currentUser == nil)
         if isNotLoggedIn {
             presentLoginVC()
         }
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(ProfileCollectionViewCell.nib,
+                                forCellWithReuseIdentifier: ProfileCollectionViewCell.identifier)
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        collectionView.collectionViewLayout = layout
         
     }
     
@@ -28,6 +62,62 @@ final class HomeViewController: UIViewController {
         DispatchQueue.main.async {
             self.present(loginVC, animated: true)
         }
+    }
+    
+    @IBAction private func sortScoreButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction private func sortRegisterButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction private func sortExperienceButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction private func filterButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction private func settingButton(_ sender: Any) {
+        
+    }
+    
+}
+
+extension HomeViewController: UICollectionViewDelegate {
+    
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        sampleData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: ProfileCollectionViewCell.identifier,
+            for: indexPath
+        ) as! ProfileCollectionViewCell
+        let model = sampleData[indexPath.row]
+        cell.configure(model: model)
+        return cell
+    }
+    
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth = self.view.frame.width
+        let cellHeight: CGFloat = 120
+        return CGSize(width: cellWidth, height: cellHeight)
     }
     
 }
---
+++ Casical/Home/ProfileCollectionViewCell.swift
@@ -0,0 +1,50 @@
+//
+//  ProfileCollectionViewCell.swift
+//  Casical
+//
+//  Created by 大西玲音 on 2021/10/29.
+//
+
+import UIKit
+
+final class ProfileCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet private weak var baseView: UIView!
+    @IBOutlet private weak var profileImageView: UIImageView!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var languageLabel: UILabel!
+    @IBOutlet private weak var houseLabel: UILabel!
+    @IBOutlet private weak var experienceLabel: UILabel!
+    
+    static var identifier: String { String(describing: self) }
+    static var nib: UINib { UINib(nibName: String(describing: self), bundle: nil) }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        baseView.layer.cornerRadius = 20
+        baseView.layer.shadowColor = UIColor.black.cgColor
+        baseView.layer.shadowOffset = CGSize(width: 2, height: 2)
+        baseView.layer.shadowRadius = 2
+        baseView.layer.shadowOpacity = 0.8
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        DispatchQueue.main.async {
+            self.profileImageView.layer.cornerRadius = self.profileImageView.frame.height / 2
+        }
+        
+    }
+    
+    func configure(model: SampleModel) {
+        profileImageView.image = model.image
+        nameLabel.text = model.name
+        languageLabel.text = "● " + model.language
+        houseLabel.text = "● " + model.house
+        experienceLabel.text = "● " + model.experience
+    }
+
+}
