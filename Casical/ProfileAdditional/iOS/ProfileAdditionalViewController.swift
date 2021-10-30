381816f7d8c3835cc4cc071231a9a4090e549054
Reon0429-cat <oonishireon110@gmail.com>
2021-10-30 19:09:59 +0900

    使う画面のstoryboardとvc作成

---
+++ Casical/ProfileAdditional/iOS/ProfileAdditionalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileAdditionalViewController.swift
+//  Casical
+//
+//  Created by 大西玲音 on 2021/10/30.
+//
+
+import UIKit
+
+class ProfileAdditionalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
